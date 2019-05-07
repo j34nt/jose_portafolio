0649e478b33f2f198a292fcf4b12cb83c6038848
Jose Jeanton <jajeantp@gmail.com>
2019-05-07 12:49:12 -0500

    Route Base HREF

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -9,6 +10,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ]
 })
 export class AppRoutingModule { }
